73fcb4cd8d752f9c6b018b1592ecd665ebab431e
Code_Andy <148534180+code-andrewy@users.noreply.github.com>
2025-05-24 03:51:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-window.location.href="https://weak-battle-stretch.glitch.me/"
+fetch("https://raw.githubusercontent.com/code-andrewy/Schoolwork/main/main.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("content").innerHTML = data;
+  })
+  .catch(error => {
+    console.error("Error loading HTML:", error);
+    document.getElementById("content").innerText = "Failed to load content.";
+  });
